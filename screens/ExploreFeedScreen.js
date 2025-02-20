38c1eb3ed0519ccb12b7d34981a5806e7b61c9b5
Crystal Shamo <crystal.shamo@gmail.com>
2025-02-19 22:14:37 -0500

    Alexa Combined This :)

---
+++ screens/ExploreFeedScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text} from 'react-native';
+
+export default function ExploreFeedScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text 
+            style={{ fontSize: 26, fontWeight: 'bold '}}>EXLORE FEED SCREEN</Text>
+      </View>
+    );
+  }
