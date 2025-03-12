960ab1124d352aef82a894173e9fa02e8fc6e289
Miray <miraynoeel079@gmail.com>
2025-03-12 17:12:17 -0400

    midterm-

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignUpScreen from "./auth/SignUpScreen";
+import LoginScreen from "./auth/LoginScreen";
+import DashboardScreen from "./auth/DashBoardScreen";
+import HomeScreen from "./HomeScreen"; 
+import PolishScreen from "./PolishScreen";
+import CollectionScreen from "./screens/CollectionScreen"; 
+import ClientAccount from "./screens/ClientAccount";
+import BusinessAccount from "./screens/BusinessAccount";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="PolishScreen" component={PolishScreen} />
+        <Stack.Screen name="CollectionScreen" component={CollectionScreen} />
+        <Stack.Screen name="ClientAccount" component={ClientAccount} />
+        <Stack.Screen name="BusinessAccount" component={BusinessAccount} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+<img width="241" alt="Screenshot 2025-03-12 at 4 59 12 PM" src="https://github.com/user-attachments/assets/9bb2ad51-5305-4a4c-b9b9-24b8327aba8b" />
+
+
+# Swatch
+
+Swatch is a nail polish collection app designed to help users organize, view, and manage their personal nail polish collections. With Swatch, users can create custom collections, add individual nail polishes, and easily search and browse through their collection based on various attributes like color, brand, and hex code.
+
+## Features
+
+- **User Accounts**: Create and manage user profiles.
+- **Create Collections**: Organize nail polishes into user-created collections.
+- **Add Nail Polishes**: Store and view individual nail polishes with details like color, brand, and hex code.
+- **View Collections**: Browse through your collections and select individual items to view more details.
+- **Search & Filter**: Easily search through collections and filter polishes by various attributes.
+
+## Technologies Used
+
+- **Frontend**: React (or specify other technologies you’re using, like HTML/CSS, etc.)
+- **Backend**: Node.js / Express (if applicable)
+- **Database**: MongoDB (or other databases you’re using)
+- **Authentication**: JWT / OAuth (if applicable)
+- **Environment Variables**: dotenv
